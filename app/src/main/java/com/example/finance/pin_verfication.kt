acb3f90a732ffada0e6b19a54b5460fe12ccb123
RiteshBorse <RiteshBorse@users.noreply.github.com>
2023-08-19 19:42:49 +0530

    Pin
    Set Pin page ui is ready

---
+++ app/src/main/java/com/example/finance/pin_verfication.kt
@@ -0,0 +1,11 @@
+package com.example.finance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pin_verfication : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pin_verfication)
+    }
+}
