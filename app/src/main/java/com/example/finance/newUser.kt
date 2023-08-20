a0c77827c13e9adb4c987fe3d7dba3e18b14a45f
RiteshBorse <RiteshBorse@users.noreply.github.com>
2023-08-20 16:08:05 +0530

    NewUser
    Taken input from the user 'name' and stored it inside a file named User.txt

--- app/src/main/java/com/example/finance/newUser.kt
+++ app/src/main/java/com/example/finance/newUser.kt
@@ -1,11 +1,42 @@
 package com.example.finance
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import java.io.File
+import android.os.Environment
 
 class newUser : AppCompatActivity() {
+    lateinit var name: EditText
+    lateinit var submit: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_user)
+
+        name = findViewById(R.id.edt_name)
+        submit = findViewById(R.id.submit_button)
+
+        // Directory and File
+        val fileName = "USER.txt"
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+        val file = File(storageDir, fileName)
+
+        if (!file.exists() || file.readText().isEmpty()) {
+            file.createNewFile()
+        }
+        else {
+            val intent = Intent(this@newUser, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Finish current activity
+        }
+
+        // Submit button
+        submit.setOnClickListener {
+            val nameOfUser = name.text.toString().trim()
+            file.writeText(nameOfUser)
+            }
+        }
     }
-}
