730e2c570996c28234ada95e01e8463ac62f8596
RiteshBorse <RiteshBorse@users.noreply.github.com>
2023-08-21 16:26:11 +0530

    Forget Pin
    Forget Pin feature added

--- app/src/main/java/com/example/finance/check_pin.kt
+++ app/src/main/java/com/example/finance/check_pin.kt
@@ -1,5 +1,6 @@
 package com.example.finance
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,18 +9,21 @@ import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import java.io.File
 
 class check_pin : AppCompatActivity() {
     lateinit var pin : EditText
     lateinit var submit : ImageButton
     lateinit var name : TextView
+    lateinit var forget_pin : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_check_pin)
         pin = findViewById(R.id.edt_pin)
         submit = findViewById(R.id.submit)
         name = findViewById(R.id.welcome_name)
+        forget_pin = findViewById(R.id.forgot_pin)
 
         //File for reference
         val fileName = "USER.txt"
@@ -50,7 +54,49 @@ class check_pin : AppCompatActivity() {
         var nameofuser = file.readText().toString()
         name.setText("Welcome, $nameofuser")
 
+        forget_pin.setOnClickListener {
+            forgetpass1()
+        }
+
+
+
+
     }
 
+    fun forgetpass1()
+    {
+        var alertDialog = AlertDialog.Builder(this@check_pin)
+        alertDialog.setTitle("Forget Pin")
+            .setMessage("Caution : All the data of the user will be erased" +
+                    " Do u want to continue")
+            .setPositiveButton("Yes",DialogInterface.OnClickListener{
+                    dialogInterface, which ->
 
+                forgetpass2()
+
+            })
+            .show()
+    }
+
+    fun forgetpass2()
+    {
+        var alertDialog2 = AlertDialog.Builder(this@check_pin)
+        alertDialog2.setTitle("Forget Pin")
+            .setMessage("Caution : Do you really want to clear all the Data")
+            .setPositiveButton("Confirm",DialogInterface.OnClickListener{
+                    dialogInterface, which ->
+
+                val fileName = "USER.txt"
+                val storageDir = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+                val file = File(storageDir, fileName)
+
+                file.delete()
+
+                val intent = Intent(this@check_pin,newUser::class.java)
+                startActivity(intent)
+
+
+            })
+            .show()
+    }
 }
